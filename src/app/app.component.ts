c33ea3ff5952abadea6113346eb71d159435842a
Tazzmanian <banskaliqtabg@yahoo.com>
2018-04-06 12:06:02 +0300

    add background and poles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,36 @@ import  * as Phaser  from 'phaser-ce';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    this.game = new Phaser.Game(
+      800, 600, Phaser.AUTO, 'content',
+      { preload: this.preload, create: this.create, update:this.update });
   }
   game: Phaser.Game;
 
   constructor() {
-    // this.game = new Phaser.Game();
   }
 
-
-
   preload() {
-    this.game.load.image('logo', 'assets/ds_logo.png');
+    //load images
+    this.game.load.image('background', 'assets/aliens.jpg');
+    this.game.load.image('pole', 'assets/pole.png');
   }
 
   create() {
-    var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    // create a new sprite to show the image on the screen
+    // var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'background');
+    var background = this.game.add.sprite(0,0, "background");
+    background.anchor.setTo(0,0);
+    var pole1 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "pole");
+    pole1.anchor.setTo(0.15, 0.5);
+    var pole2 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "pole");
+    pole2.anchor.setTo(0.5);
+    var pole3 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "pole");
+    pole3.anchor.setTo(0.85, 0.5);
+  }
+
+  update() {
+
   }
 
 }
